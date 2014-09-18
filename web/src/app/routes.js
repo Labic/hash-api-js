ffade1afe4c452ed33af82b829055257d33f3e4c
Gustavo RPS <gustavorps@gustavorps.net>
2014-09-18 18:50:36 -0300

    Implementado a autenticação Oauth para Facebook

--- web/src/app/routes.js
+++ web/src/app/routes.js
@@ -17,13 +17,6 @@ module.exports = function(app, passport) {
 		res.render('login', { message: req.flash('loginMessage') }); 
 	});
 
-	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-	}));
-
 	// =====================================
 	// SIGNUP ==============================
 	// =====================================
@@ -35,7 +28,24 @@ module.exports = function(app, passport) {
 	});
 
 	// process the signup form
-	// app.post('/signup', do all our passport stuff here);
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+	
+	// =====================================
+	// FACEBOOK ROUTES =====================
+	// =====================================
+	// route for facebook authentication and login
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
 
 	// =====================================
 	// PROFILE SECTION =====================
---
+++ web/src/config/auth.js
@@ -0,0 +1,24 @@
+// config/auth.js
+// TODO: Make app domain dynamic
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '1470450163242397', // your App ID
+		'clientSecret' 	: 'e1e0465a113fe1281660a31c716a3157', // your App Secret
+		'callbackURL' 	: 'http://app-enem.dev.inep.gov.br:3000/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
+		'callbackURL' 		: 'http://app-enem.dev.inep.gov.br:3000/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://app-enem.dev.inep.gov.br:3000/auth/google/callback'
+	}
+
+};
