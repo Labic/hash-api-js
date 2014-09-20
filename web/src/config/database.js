def69dc75da1b25e6334d6769a41faab4da94f40
Gustavo RPS <gustavorps@gustavorps.net>
2014-09-19 21:34:05 -0300

    Ajuste na conexão do banco de dados

--- web/src/config/database.js
+++ web/src/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost/app-enem' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://localhost/app-enem-web' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
