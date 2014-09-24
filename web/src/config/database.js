6b1ea9ac5522e633051eb713a4d02ad7fccf8c47
Gustavo RPS <gustavorps@gustavorps.net>
2014-09-24 00:08:13 -0300

    Troca do servido de banco de dados MongoDB local para MongoLab e ajuste no template do twitter.jade

--- web/src/config/database.js
+++ web/src/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost/app-enem-web' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://app-enem-web-dev:app-enem-web-dev@ds033740.mongolab.com:33740/app-enem-web-dev' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
