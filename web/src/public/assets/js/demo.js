8c6925d81714e6d491924afca47151a40845c72b
Gustavo RPS <gustavorps@gustavorps.net>
2014-10-01 20:35:46 -0300

    Implementação completa da rolagem estilo TweetDeck

--- web/src/public/assets/js/demo.js
+++ web/src/public/assets/js/demo.js
@@ -1,33 +1,43 @@
-$(document).ready(function() {	
-	moment.locale('pt-BR');
+$(document).ready(function () {	
+    moment.locale('pt-BR');
 
-	$('.tweets').height($(window).height() - 200);
+    $('.scrollbar').height($(window).height() - 170);
 
-	var tweetsScroll = $('.tweets').niceScroll();
+    $.template('tweetUpdateTemplate', '<div class="p-t-10 p-b-5 b-b b-grey"><div class="post"><div class="user-profile-pic-wrapper"><div class="user-profile-pic-2x white-border"><a href="https://twitter.com/${status.user.screen_name}" target="_blank" data-toggle="tooltip" data-placement="top" title="${status.user.screen_name} - ${status.user.followers_count} Seguidores"><img width="45" height="45" src="${status.user.profile_image_url_https}" alt="${status.user.screen_name}"></a></div></div><div class="info-wrapper inline"><div class="info text-black"><p>${status.text}</p><p class="muted small-text">${status.created_at} | ${rts} Retweets </p></div></div><div class="clearfix"></div></div></div>');
 
-	$('a.load-more').on('click', function () {
-		var loadMore = $(this);
-		var filter = $.parseJSON(getParameterByName('filter', $(loadMore).data('uri')));
-		filter.skip = $(loadMore).data('skip');
-		$(loadMore).data('skip', filter.skip + 10);
+    $('.scrollbar').each(function () {
+        var tweetsToLoad = this;
+        var scroll = $(tweetsToLoad).niceScroll();
 
-		$.getJSON('http://104.131.228.31:3000/api/Tweets?filter=' + $.stringify(filter), function (tweets) {
-			$.template('tweetUpdateTemplate', '<div class="p-t-20 p-b-15 b-b b-grey"><div class="post overlap-left-10"><div class="user-profile-pic-wrapper"><div class="user-profile-pic-2x white-border"><a href="https://twitter.com/${status.user.screen_name}" target="_blank" data-toggle="tooltip" data-placement="top" title="${status.user.screen_name} - ${status.user.followers_count} Seguidores"><img width="45" height="45" src="${status.user.profile_image_url_https}" alt="${status.user.screen_name}"></a></div></div><div class="info-wrapper inline"><div class="info text-black"><p>${status.text}</p><p class="muted small-text">${status.created_at} | ${rts} Retweets</p></div></div><div class="clearfix"></div></div></div>');
-			for (x in tweets) {
-				tweets[x].status.created_at = moment(tweets[x].status.created_at).fromNow();
-			}
-			//TODO: Refatore isso!
-			$.tmpl('tweetUpdateTemplate', tweets).appendTo($(loadMore).parent().parent().parent().children('.tweets'));
-		});
-		
-		return false;
-	});
-}); 
+        scroll.scrollend(function () {
+            if (this.scrollvaluemax <= this.scroll.y + 5) {
+                var filter = $.parseJSON(getParameterByName('filter', $(tweetsToLoad).data('uri')));
+                filter.skip = $(tweetsToLoad).data('skip'); 
 
-$(window).resize(function() {
+                if ($(tweetsToLoad).data('all-load') !== true) {
+                    $.getJSON('http://104.131.228.31:3000/api/Tweets?filter=' + $.stringify(filter), function (tweets) {
+                        if (tweets.length > 0) {
+                            for (x in tweets) {
+                                tweets[x].status.created_at = moment(tweets[x].status.created_at).fromNow();
+                            }
 
+                            //TODO: Refatore isso!
+                            $.tmpl('tweetUpdateTemplate', tweets).appendTo(tweetsToLoad);
+                            
+                            $(tweetsToLoad).data('skip', filter.skip + 10);
+                        } else {
+                            $(tweetsToLoad).data('all-load', true);
+                        }
+                    });
+                }
+            }
+        });
+    });
 });
 
+$(window).resize(function () {
+    $('.scrollbar').height($(window).height() - 170);
+});
 
 function getParameterByName(name, url) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
