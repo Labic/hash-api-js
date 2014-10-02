679c1949e962874302cb1e3053fcea589a82b599
Gustavo RPS <gustavorps@gustavorps.net>
2014-10-02 02:24:14 -0300

    Adicionado o modelo de API para os posts do Facebook e ajustes e padronização nos códigos

--- api/src/common/models/tweet.js
+++ api/src/common/models/tweet.js
@@ -1,3 +1,13 @@
 module.exports = function(Tweet) {
-	
+	Tweet.Favorite = function (statusId, callback) {
+		callback(null, true);
+	}
+
+	Tweet.remoteMethod(
+		'Favorite',
+		{
+			accepts: [{arg: 'statusId', type: 'number'}],
+			returns: {arg: 'success', type: 'boolean'}
+		}
+	);
 };
