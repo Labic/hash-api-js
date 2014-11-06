55f83c1ade24c76abfd91085c691adc06d74c7ae
Gustavo RPS <gustavorps@gustavorps.net>
2014-11-06 14:55:19 -0200

    Restruturando repositório

--- web/src/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-	'url' : 'mongodb://app-enem-web-dev:app-enem-web-dev@ds033740.mongolab.com:33740/app-enem-web-dev' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
