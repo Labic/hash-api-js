b7aae5a5aead4d887c6d5d18522cff71adf0dde6
Gustavo RPS <dev@gustavorps.net>
2015-07-22 23:25:57 -0300

    Updated plural of models

--- common/models/lgbt.js
+++ common/models/lgbt.js
@@ -1,18 +1,3 @@
-var TopLGBT = require('./top-lgbt');
-
-module.exports = function(LGBT) {
-  //var TopLGBT = app.models.TopLGBT;
-
-  LGBT.topMentions = function(cb) {
-    cb(null, {name : "NotImplementedError", message : "too lazy to implement"});
-  };
-
-  LGBT.remoteMethod(
-    'topMentions',
-    {
-      description: 'Get top 20 mentions for LGBT on Twitter',
-      http: { path: '/top/mentions', verb: 'get' },
-      returns: { type: 'array', root: true }
-    }
-  );
+module.exports = function(lgbt) {
+  
 };
