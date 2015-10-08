dadb043ca76322c519ad57d3df7c18cb871543ba
GustavoRPS <email@gustavorps.net>
2015-10-08 18:54:45 -0300

    Refactory Twitter Metrics and created Metric model to implement the metrics of other models

--- common/models/tweet.js
+++ common/models/tweet.js
@@ -9,54 +9,8 @@ module.exports = function(Tweet) {
     http: {path: '/analytics', verb: 'get'}
   });
 
-  Tweet.remoteMethod('metrics', {
-    accepts: [
-      {arg: 'name', type: 'string', required: true },
-      {arg: 'since', type: 'string', required: true },
-      {arg: 'until', type: 'string', required: true },
-      {arg: 'tags', type: 'string' }, // Categories alias
-      {arg: 'hashtags', type: 'string' }
-    ],
-    returns: {type: 'object', root: true},
-    http: {path: '/metrics', verb: 'get'}
-  });
-
   Tweet.analytics = function(type, filter, callback) {
     Tweet.analytics.mongodb(type, filter, callback);
-  }
-
-  Tweet.metrics = function(name, since, until, tags, hashtags, cb) {
-    var query = {
-      'status.timestamp_ms': {
-        $gte: new Date(since).getTime(),
-        $lte: new Date(until).getTime()
-      }
-    }
-
-    if (tags)
-      query.categories = { $all: tags.split(',') };
-      //query.categories = { $all: tags.replace(/ /g,'').split(',') };
-
-    if (hashtags)
-      query['status.entities.hashtags.text'] = { $all: hashtags.replace(/ /g,'').split(',') };
-    
-    if (query.$gte == null || query.$lte == null) {
-      var err = new Error('Malformed request syntax. Check the query string arguments!');
-      err.statusCode = 400;
-
-      return cb(err);
-    }
-
-    console.log('metric?name=count \n %j', query);
-
-    return Tweet.mongodb.count(query, function(err, result) {
-      console.log(result);
-      result = {
-        count: result
-      };
-
-      cb(err, result)
-    });
   }
 
 };
---
+++ server/models/metric.js
@@ -0,0 +1,153 @@
+module.exports = function(Metric) {
+
+  Metric.remoteMethod('tweetsMetrics', {
+    accepts: [
+      {arg: 'name', type: 'String', required: true },
+      {arg: 'since', type: 'Date', required: true },
+      {arg: 'until', type: 'Date', required: true },
+      {arg: 'tags', type: 'String' }, // Categories alias
+      {arg: 'hashtags', type: 'String' }
+    ],
+    returns: {type: 'object', root: true},
+    http: {path: '/tweets/:name', verb: 'get'}
+  });
+
+  Metric.tweetsMetrics = function(name, since, until, tags, hashtags, cb) {
+    tweetsMetricsMethods[name](since, until, tags, hashtags, cb);
+  }
+
+  var tweetsMetricsMethods = {};
+  
+  tweetsMetricsMethods['count'] = function (since, until, tags, hashtags, cb) { 
+    if (isNaN(since.getTime()) || isNaN(until.getTime())) {
+      var err = new Error('Malformed request syntax. Check the query string arguments!');
+      err.statusCode = 400;
+
+      return cb(err);
+    }
+
+    var query = {
+      'status.timestamp_ms': {
+        $gte: since.getTime(),
+        $lte: until.getTime()
+      }
+    };
+
+    if (tags)
+      query.categories = { $all: tags.split(',') };
+      // query.categories = { $all: tags.replace(/ /g,'').split(',') };
+
+    if (hashtags)
+      query['status.entities.hashtags.text'] = { $all: hashtags.replace(/ /g,'').split(',') };
+
+    console.log('/tweets/metrics/count \n %j', query);
+
+    var Tweet = Metric.app.models.Tweet;
+    return Tweet.mongodb.count(query, function(err, result) {
+      if (err) 
+        return cb(err, null);
+      else 
+        return cb(err, result);
+    });
+  };
+
+  tweetsMetricsMethods['count_images'] = function (since, until, tags, hashtags, cb) { 
+    if (isNaN(since.getTime()) || isNaN(until.getTime())) {
+      var err = new Error('Malformed request syntax. Check the query string arguments!');
+      err.statusCode = 400;
+
+      return cb(err);
+    }
+
+    var pipeline = [
+      { $match: {
+        'status.entities.media.0': { $exists: true },
+        'status.timestamp_ms': {
+          $gte: since.getTime(),
+          $lte: until.getTime()
+        }
+      } },
+      { $group: {
+        _id: 0,
+        count: { $sum: 1 }
+      } },
+      { $project: {
+        _id: 0,
+        count: '$count'
+      } }
+    ];
+
+    if (tags)
+      pipeline[0].$match['categories'] = { $all: tags.split(',') };
+      // pipeline[0].$match['categories'] = { $all: tags.replace(/ /g,'').split(',') };
+
+    if (hashtags)
+      pipeline[0].$match['status.entities.hashtags.text'] = { $all: hashtags.replace(/ /g,'').split(',') };
+
+    console.log('/tweets/metrics/count_images \n %j', pipeline);
+
+    var Tweet = Metric.app.models.Tweet;
+    return Tweet.aggregate(pipeline, function(err, result) {
+      console.log(result);
+
+      if (err) 
+        return cb(err, null);
+      else 
+        return cb(null, result[0]);
+    });
+  }
+
+  tweetsMetricsMethods['geolocation'] = function (since, until, tags, hashtags, cb) { 
+    if (isNaN(since.getTime()) || isNaN(until.getTime())) {
+      var err = new Error('Malformed request syntax. Check the query string arguments!');
+      err.statusCode = 400;
+
+      return cb(err);
+    }
+
+    var pipeline = [
+      { $match: {
+        $or: [ { 'status.geo': { $ne: null } }, 
+               { 'city_geo': { $exists: true } } ]
+      } }, 
+      { $group: {
+        _id: 0,
+        features: { $push: { 
+          type: {  $literal: 'Feature' },
+          properties: {
+            id_str: '$status.id_str',
+          },
+          geometry: {
+            type: {  $literal: 'Point' }, 
+            coordinates: { $cond: [ { $ne: [ '$status.coordinates', null ] }, '$status.geo.coordinates', '$city_geo' ] }
+          }
+        } }
+      } },
+      { $project: {
+        _id: 0,
+        type: {  $literal: 'FeatureCollection' },
+        features: '$features'
+      } }
+    ];
+
+    if (tags)
+      pipeline[0].$match['categories'] = { $all: tags.split(',') };
+      // pipeline[0].$match['categories'] = { $all: tags.replace(/ /g,'').split(',') };
+
+    if (hashtags)
+      pipeline[0].$match['status.entities.hashtags.text'] = { $all: hashtags.replace(/ /g,'').split(',') };
+
+    console.log('/tweets/metrics/count_images \n %j', pipeline);
+
+    var Tweet = Metric.app.models.Tweet;
+    return Tweet.aggregate(pipeline, function(err, result) {
+      console.log(result);
+
+      if (err) 
+        return cb(err, null);
+      else 
+        return cb(null, result[0]);
+    });
+  }
+
+};
