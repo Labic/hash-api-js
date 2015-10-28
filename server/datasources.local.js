fee6748f6e4e6fad55df26df7e88e95903c11f58
GustavoRPS <email@gustavorps.net>
2015-10-28 20:32:47 -0200

    Ajust of model and data sourcers

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,5 +1,8 @@
 module.exports = {
   db: {
+    connector: 'memory'
+  },
+  documentDB: {
     connector: 'mongodb',
     url: process.env.MONGODB_URI || 'mongodb://localhost/hash',
     allowExtendedOperators: true
