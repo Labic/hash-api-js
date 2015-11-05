5c24bfa22f7e7fe4930cbbd1a5fbdd3ac31e589e
GustavoRPS <email@gustavorps.net>
2015-11-05 15:37:11 -0200

    more pog bug fix

--- api/models/analytic.js
+++ api/models/analytic.js
@@ -477,7 +477,7 @@ module.exports = function(Analytic) {
         text: '$status_text',
         user: {
             // TODO: THIS IS WRONG BECAUSE "SOMEONE" MAKE WRONG IN ANOTER API ENDPOINT
-            id_str: '$_id.id_str'
+            id_str: '$_id.id_str',
             // id_str: '$user_id_str',
             screen_name: '$user_screen_name',
             profile_image_url_https: '$user_profile_image_url_https'
