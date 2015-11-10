90a94ed315a264fede85506ecaec0c88be8b06a7
GustavoRPS <email@gustavorps.net>
2015-11-10 15:13:00 -0200

    added ISO 8601 durations patterns

--- api/models/enums/cacheTTLenum.js
+++ api/models/enums/cacheTTLenum.js
@@ -8,4 +8,13 @@ cacheTTLenum['1d']  = 6 * 60 * 60 * 1000;
 cacheTTLenum['7d']  = 6 * 60 * 60 * 1000;
 cacheTTLenum['15d'] = 6 * 60 * 60 * 1000;
 
+cacheTTLenum['PT15M'] = 15 * 60 * 1000;
+cacheTTLenum['PT30M'] = 30 * 60 * 1000;
+cacheTTLenum['P1H']   = 60 * 60 * 1000;
+cacheTTLenum['P12H']  = 12 * 60 * 60 * 1000;
+cacheTTLenum['P1D']   = 24 * 60 * 60 * 1000;
+cacheTTLenum['P7D']   = 7 * 24 * 60 * 60 * 1000;
+cacheTTLenum['P15D']  = 15 * 24 * 60 * 60 * 1000;
+cacheTTLenum['P1M']   = 30 * 24 * 60 * 60 * 1000;
+
 module.exports = cacheTTLenum;
--- api/models/enums/periodEnum.js
+++ api/models/enums/periodEnum.js
@@ -8,4 +8,14 @@ periodEnum['1d']   = 24 * 60 * 60 * 1000;
 periodEnum['7d']   = 7 * 24 * 60 * 60 * 1000;
 periodEnum['15d']  = 15 * 24 * 60 * 60 * 1000;
 
+// TODO: Use new period convention
+periodEnum['PT15M'] = 15 * 60 * 1000;
+periodEnum['PT30M'] = 30 * 60 * 1000;
+periodEnum['P1H']   = 60 * 60 * 1000;
+periodEnum['P12H']  = 12 * 60 * 60 * 1000;
+periodEnum['P1D']   = 24 * 60 * 60 * 1000;
+periodEnum['P7D']   = 7 * 24 * 60 * 60 * 1000;
+periodEnum['P15D']  = 15 * 24 * 60 * 60 * 1000;
+periodEnum['P1M']   = 30 * 24 * 60 * 60 * 1000;
+
 module.exports = periodEnum;
