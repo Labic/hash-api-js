a6ece6c1514a958a247e99c6252058de889c3168
GustavoRPS <email@gustavorps.net>
2015-11-11 16:50:44 -0200

    added more options to enums

--- api/models/enums/cacheTTLenum.js
+++ api/models/enums/cacheTTLenum.js
@@ -1,8 +1,9 @@
+// https://en.wikipedia.org/wiki/ISO_8601#Durations
 var cacheTTLenum = {};
 cacheTTLenum['15m'] = 15 * 60 * 1000;
 cacheTTLenum['30m'] = 30 * 60 * 1000;
 cacheTTLenum['1h']  = 60 * 60 * 1000;
-cacheTTLenum['6h']  = 6 * 60 * 60 * 1000;
+cacheTTLenum['6h']  = 60 * 60 * 1000;
 cacheTTLenum['12h'] = 6 * 60 * 60 * 1000;
 cacheTTLenum['1d']  = 6 * 60 * 60 * 1000;
 cacheTTLenum['7d']  = 6 * 60 * 60 * 1000;
@@ -11,6 +12,7 @@ cacheTTLenum['15d'] = 6 * 60 * 60 * 1000;
 cacheTTLenum['PT15M'] = 15 * 60 * 1000;
 cacheTTLenum['PT30M'] = 30 * 60 * 1000;
 cacheTTLenum['P1H']   = 60 * 60 * 1000;
+cacheTTLenum['P6H']   = 60 * 60 * 1000;
 cacheTTLenum['P12H']  = 12 * 60 * 60 * 1000;
 cacheTTLenum['P1D']   = 24 * 60 * 60 * 1000;
 cacheTTLenum['P7D']   = 7 * 24 * 60 * 60 * 1000;
