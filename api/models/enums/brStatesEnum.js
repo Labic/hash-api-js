355c95e254d25ec362e14702c26541c9c3ccfe50
GustavoRPS <email@gustavorps.net>
2015-11-17 01:25:48 -0200

    Added Brazilian States ENUM (ISO, FIPS and HASC) and cleaning code

---
+++ api/models/enums/brStatesEnum.js
@@ -0,0 +1,139 @@
+// TODO: Implement microregions http://www.statoids.com/ybr.html
+module.exports = {
+      { DF: {
+    name: 'Distrito Federal',
+    fips: 'BR07',
+    iso: 'BR-DF',
+    hasc: 'BR.DF'
+  } },{ AC: {
+    name: 'Acre',
+    fips: 'BR01',
+    iso: 'BR-AC',
+    hasc: 'BR.AC'
+  } },{ AL: {
+    name: 'Alagoas',
+    fips: 'BR02',
+    iso: 'BR-AL',
+    hasc: 'BR.AL'
+  } },{ AP: {
+    name: 'Amapá',
+    fips: 'BR03',
+    iso: 'BR-AP',
+    hasc: 'BR.AP'
+  } },{ AM: {
+    name: 'Amazonas',
+    fips: 'BR04',
+    iso: 'BR-AM',
+    hasc: 'BR.AM'
+  } },{ BA: {
+    name: 'Bahia',
+    fips: 'BR05',
+    iso: 'BR-BA',
+    hasc: 'BR.BA'
+  } },{ CE: {
+    name: 'Ceará',
+    fips: 'BR06',
+    iso: 'BR-CE',
+    hasc: 'BR.CE'
+  } },{ ES: {
+    name: 'Espírito Santo',
+    fips: 'BR08',
+    iso: 'BR-ES',
+    hasc: 'BR.ES'
+  } },{ GO: {
+    name: 'Goiás',
+    fips: 'BR29',
+    iso: 'BR-GO',
+    hasc: 'BR.GO'
+  } },{ MA: {
+    name: 'Maranhão',
+    fips: 'BR13',
+    iso: 'BR-MA',
+    hasc: 'BR.MA'
+  } },{ MT: {
+    name: 'Mato Grosso',
+    fips: 'BR15',
+    iso: 'BR-MT',
+    hasc: 'BR.MT'
+  } },{ MS: {
+    name: 'Mato Grosso do Sul',
+    fips: 'BR11',
+    iso: 'BR-MS',
+    hasc: 'BR.MS'
+  } },{ MG: {
+    name: 'Minas Gerais',
+    fips: 'BR15',
+    iso: 'BR-MG',
+    hasc: 'BR.MG'
+  } },{ PA: {
+    name: 'Pará',
+    fips: 'BR16',
+    iso: 'BR-PA',
+    hasc: 'BR.PA'
+  } },{ PB: {
+    name: 'Paraíba',
+    fips: 'BR17',
+    iso: 'BR-PB',
+    hasc: 'BR.PB'
+  } },{ PR: {
+    name: 'Paraná',
+    fips: 'BR18',
+    iso: 'BR-PR',
+    hasc: 'BR.PR'
+  } },{ PE: {
+    name: 'Pernambuco',
+    fips: 'BR30',
+    iso: 'BR-PE',
+    hasc: 'BR.PE'
+  } },{ PI: {
+    name: 'Piauí',
+    fips: 'BR20',
+    iso: 'BR-PI',
+    hasc: 'BR.PI'
+  } },{ RJ: {
+    name: 'Rio de Janeiro',
+    fips: 'BR21',
+    iso: 'BR-RJ',
+    hasc: 'BR.RJ'
+  } },{ RN: {
+    name: 'Rio Grande do Norte',
+    fips: 'BR22',
+    iso: 'BR-RN',
+    hasc: 'BR.RN'
+  } },{ RS: {
+    name: 'Rio Grande do Sul',
+    fips: 'BR23',
+    iso: 'BR-RS',
+    hasc: 'BR.RS'
+  } },{ RO: {
+    name: 'Rondônia',
+    fips: 'BR24',
+    iso: 'BR-RO',
+    hasc: 'BR.RO'
+  } },{ RR: {
+    name: 'Roraima',
+    fips: 'BR25',
+    iso: 'BR-RR',
+    hasc: 'BR.RR'
+  } },{ SC: {
+    name: 'Santa Catarina',
+    fips: 'BR26',
+    iso: 'BR-SC',
+    hasc: 'BR.SC'
+  } },{ SP: {
+    name: 'São Paulo',
+    fips: 'BR27',
+    iso: 'BR-SP',
+    hasc: 'BR.SP'
+  } },{ SE: {
+    name: 'Sergipe',
+    fips: 'BR28',
+    iso: 'BR-SE',
+    hasc: 'BR.SE'
+  } },{ TO: {
+    name: 'Tocantins',
+    fips: 'BR31',
+    iso: 'BR-TO',
+    hasc: 'BR.TO'
+  } }
+};
