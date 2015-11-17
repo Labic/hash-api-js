0cdb6febe636f35775b0e4391e5e51955a50a3cd
GustavoRPS <email@gustavorps.net>
2015-11-17 20:59:38 -0200

    implemented Analytics for Instagram Most Liked Media MongoDB DAO

---
+++ api/dao/mongodb/analytics-instagram/most-liked-medias.js
@@ -0,0 +1,36 @@
+module.exports = function mostLikedMedias(params, model, cb) { 
+  var query = {
+    'data.created_time': {
+      $gte: params.since.getTime(),
+      $lte: params.until.getTime()
+    }
+  }
+
+  var options = {
+    sort: [['data.likes.count', -1]],
+    limit: params.perPage * params.page,
+    skip: (params.perPage * params.page) - params.perPage
+  }
+
+  if(params.filter.tags) {
+    if(params.filter.tags.with)
+      query['categories'] = { $all: params.filter.tags.with };
+
+    if(params.filter.tags.contains)
+      query['categories'] = { $in: params.filter.tags.contains };
+  }
+
+  if(params.filter.hashtags)
+    query['data.tags'] = { $in: params.filter.hashtags };
+
+  if(params.filter.mentions)
+    query['data.users_in_photo.username'] = { $in: params.filter.mentions };
+
+  if(params.filter.users)
+    query['data.user.username'] = { $in: params.filter.users };
+
+  if(params.filter.type)
+    query['data.type'] = { $in: params.filter.type };
+
+  model.dao.mongodb.find(query, options, cb);
+};
