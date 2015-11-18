e336d20f76515d079adffebfe2636dc0d21849c0
GustavoRPS <email@gustavorps.net>
2015-11-17 22:05:00 -0200

    added TODO for /analytics/twitter

--- api/dao/mongodb/analytics-twitter/index.js
+++ api/dao/mongodb/analytics-twitter/index.js
@@ -2,6 +2,7 @@ module.exports = {
   geolocation: require('./geolocation'),
   mostActiveUsers: require('./most-active-users'),
   mostMentionedUsers: require('./most-mentioned-users'),
+  // TODO: Rename mostPopularHashtags to mostFrequentHashtags
   mostPopularHashtags: require('./most-popular-hashtags'),
   mostRecentlyRetweetedTweets: require('./most-recently-retweeted-tweets'),
   mostRetweetedTweets: require('./most-retweeted-tweets'),
