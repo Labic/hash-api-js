213561d7341e0db3a3416e58156dcd71fa80c5a1
GustavoRPS <email+tech@gustavorps.net>
2017-08-01 17:27:01 -0300

    added config local

---
+++ api/config.local.js
@@ -0,0 +1,31 @@
+var p = require('../package.json'),
+    version = p.version.split('.').shift();
+
+module.exports = {
+  isDevEnv: (process.env.NODE_ENV === 'development'),
+  restApiRoot: (process.env.REST_API_ROOT || '/v' + version),
+  host: process.env.IP || '0.0.0.0',
+  port: (process.env.PORT || 3000),
+  remoting: {
+    context: false,
+    rest: {
+      handleErrors: true,
+      normalizeHttpPath: false,
+      xml: false
+    },
+    json: {
+      strict: false,
+      limit: '500kb'
+    },
+    urlencoded: {
+      extended: true,
+      limit: '80kb'
+    },
+    cors: false,
+    errorHandler: {
+      disableStackTrace: false
+    }
+  },
+  livereload: process.env.LIVE_RELOAD || false,
+  legacyExplorer: process.env.LEGACY_EXPLORER || false
+};
