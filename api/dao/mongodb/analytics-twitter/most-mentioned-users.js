5052fc8c14d64c8478f9f54eed965265bf9bd214
Gustavo RPS <email+gitkraken.com@gustavorps.net>
2017-11-06 18:17:05 -0200

    implemented filter[quoted_status] param at /analytics/twitter/most_mentioned_users

--- api/dao/mongodb/analytics-twitter/most-mentioned-users.js
+++ api/dao/mongodb/analytics-twitter/most-mentioned-users.js
@@ -49,6 +49,9 @@ module.exports = function mostMentionedUsers(params, model, cb) {
   if(params.filter.retweeted)
     pipeline[0].$match['status.retweeted_status'] = { $exists: params.filter.retweeted };
 
+  if(_.isBoolean(params.filter.quoted_status))
+    pipeline[0].$match['status.quoted_status'] = { $exists: params.filter.quoted_status };
+
   if(_.isBoolean(params.filter.blocked))
     pipeline[0].$match['block'] = params.filter.blocked;
 
